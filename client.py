b6fa800f89acd52e970ee6c62141a2f42b8aa06c
Wan Afifi Wan Zain <epy770@gmail.com>
2021-12-30 17:08:03 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,80 @@
+import socket
+import sys
+from types import TracebackType
+
+HEADER = 1024
+PORT = 8501 ##Keyence port number need to check with KV Studio at Unit Editor --> check socket function need to be used --> Check Port No. (host link) --> 8501
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "10.30.34.51"
+ADDR = (SERVER, PORT)
+
+def exit_ka(get):
+    if get == "y":
+        start()
+    else:
+        sys.exit()
+
+def start():
+    try:
+        global client
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(ADDR)
+        # print(client)
+        # print(type(client))
+        print(f"[CONNECTED] to {SERVER} port {PORT} -->> \|^_^|/ ")
+        return main()
+    except :
+        get = input(f"[NO CONNECTION] SERVER:{SERVER} PORT:{PORT} -->> (-_-!)\nWant to Retry? :")
+        return exit_ka(get)
+    
+
+def hextostring(data):
+    hex_string = data
+    bytes_object = bytes.fromhex(hex_string)
+    ascii_string = bytes_object.decode("ASCII")
+    return ascii_string
+
+def upperEverything(thingsToUpper):
+    totalchar = ""
+    for char in thingsToUpper:
+        if char.isnumeric() == False:
+            temp = char
+            char = temp.upper()
+            
+        elif char.isnumeric() == True:
+            pass
+
+        else:
+            pass
+        totalchar = totalchar + char
+
+    return totalchar
+
+def send(msg):
+    print(msg)
+    message = msg.encode(FORMAT)
+    client.send(message)
+    datareceived = client.recv(2048).decode(FORMAT)
+    if msg == "RDS DM10268.H 68\r":
+        data = hextostring(datareceived)
+        print(f"Response : {data}")
+        print(len(data))
+        return main()
+    else:
+        print(f"Response : {datareceived}")
+        print(type(datareceived))
+        return main()
+
+def main():
+    getdata = input("Please input Command : ")
+    getdata = upperEverything(getdata)
+    
+    if getdata == "EXIT":
+        sys.exit()
+    elif getdata == "PROGRAM MODE":
+        send("M\r")
+    else:        
+        send(getdata + "\r")
+
+start()
